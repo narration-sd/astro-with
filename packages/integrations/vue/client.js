a7084fecd33364701daef3bb565afee7c00f36d9
nsd <narrationsd@gmail.com>
2022-07-29 17:30:54 -0700

    first we need to get the arrangement working better with the rc changes. client.js mods help. The mod.d.ts change is ide autofix to a CRLF commit they made.

--- packages/integrations/vue/client.js
+++ packages/integrations/vue/client.js
@@ -13,8 +13,9 @@ export default (element) =>
 			slots[key] = () => h(StaticHtml, { value, name: key === 'default' ? undefined : key });
 		}
 		if (client === 'only') {
+			console.log('ABOUT TO CREATE app')
 			const app = createApp({ name, render: () => h(Component, props, slots) });
-			console.log('ABOUT TO PREPARE en-suite')
+			console.log('ABOUT TO PREPARE en-suite B')
 			import ('../../../src/modules/ext-vue-prepare.mjs')
 				.then (prepare => {
 					console.log ('FROM EST-VUE-PREPARE: preparing...')
@@ -22,14 +23,15 @@ export default (element) =>
 					console.dir(prepare)
 					// console.log ('stringified prepare: ' + JSON.stringify(prepare))
 					// console.log ('prepare: ' + prepare)
-					return prepare.default (app, 'vue client')
+					return prepare.default (app, 'vue-client')
 				})
-				.catch (err => {
-					// *todo* either this goes silent, or we always ssrequire a prepare file
-					console.error ('PREPARE IMPORT or PREPARE failed: ' + err)
-				})
-				.then (() => {
-					console.log ('CLIENTJS ABOUT TO MOUNT THIS ELEMENT')
+				// .catch (err => {
+				// 	// *todo* either this goes silent, or we always require a prepare file
+				// 	console.error ('PREPARE IMPORT or PREPARE failed: ' + err)
+				// })
+				.then ((result) => {
+					console.log('PRE-MOUNT prepare result: ' + result)
+					console.log ('CLIENTJS ABOUT TO MOUNT THIS ELEMENT: result')
 					app.mount(element, false);
 				})
 				.catch (err => {

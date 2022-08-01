dc0d69b0338b7aa269cba559cd070a92b3658480
nsd <narrationsd@gmail.com>
2022-07-31 21:09:00 -0700

    this is mainly about better commenting

--- packages/integrations/vue/client.js
+++ packages/integrations/vue/client.js
@@ -13,13 +13,31 @@ export default (element) =>
 			slots[key] = () => h(StaticHtml, { value, name: key === 'default' ? undefined : key });
 		}
 		if (client === 'only') {
-			// n.b. this is from eventual module location...
-			// also, if you should change the path, watch out for browser and
-			// dev environment problems. Clear caches, restart both, or build-preview.
+			// What we are doing here is allowing an appropriate prepare script to create the
+			// app, so that it can add its own adjuncts, typically via app.use(). If this fails,
+			// or if appropriately named no prepare/integration--prepare.mjs script exists on 
+			// the client, we simply revert to the original creation of the app here. 
+			
+			// n.b. should you ever change the path, watch out for browser and dev environment caching
+			// problems. Clear caches, restart both, then do a build-preview run intially, to avoid.
+			
+			// note that you can't do this, must hard-code the path, as otherwise vite-rollup 
+			// won't recognize the string asa path, either to include its referred file in 
+			// the build dist chunks, or to uuid it. 
+			// 
+			//     const prepare_file = '../../../prepare/vue-prepare.mjs'
+			//     import (prepare_file)
+			//
+			// The good side of how this does work is that build or running under dev run will 
+			// fail on missing the prepare, or if you miss-spell its name, giving a hard warning..
+			
 			import ('../../../prepare/vue-prepare.mjs')
+				.catch (err => {
+					throw new Error ('prepare script not present: ' + err)
+				})
 				.then (prepare => {
 					const createArgs = { h, Component, props, slots };
-					return prepare.default (name, createArgs);
+					return prepare.default (name, createArgs); // .default because of import()
 				})
 				.catch (err => {
 					console.log ('CLIENTJS:prepare failed:' + err.stack)
@@ -32,13 +50,11 @@ export default (element) =>
 				.catch (err => {
 					// *todo* condition logging on what kind of error -- not loading, no, normal?
 					console.log('CLIENTJS:failed prepare: ' + err)
-					console.log('CLIENTJS ABOUT TO CREATE OWN app')
+					console.log('CLIENTJS ABOUT TO CREATE OWN ELEMENT')
 					const app = createApp({ name, render: () => h(Component, props, slots) })
 					console.log ('CLIENTJS ABOUT TO MOUNT OWN ELEMENT: result')
 					app.mount(element, false);
 				})
-			
-
 		} else {
 			const app = createSSRApp({ name, render: () => h(Component, props, slots) });
 			app.mount(element, true);
